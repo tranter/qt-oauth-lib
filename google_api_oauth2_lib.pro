789b689ca418640e387aed9388cada6a1fc6dec5
Igor Grivko <igrivko@ics.com>
2012-03-22 00:24:55 +0400

    Initial commit.

---
+++ google_api_oauth2_lib.pro
@@ -0,0 +1,31 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-01-18T16:34:48
+#
+#-------------------------------------------------
+
+QT       += webkit
+
+TARGET = google_api_oauth2_lib
+TEMPLATE = lib
+CONFIG += staticlib
+
+SOURCES += oauth2.cpp \
+    logindialog.cpp
+
+HEADERS += oauth2.h \
+    logindialog.h
+unix:!symbian {
+    maemo5 {
+        target.path = /opt/usr/lib
+    } else {
+        target.path = /usr/lib
+    }
+    INSTALLS += target
+}
+
+FORMS += \
+    logindialog.ui
+
+
+
---
+++ oauth2.h
@@ -0,0 +1,48 @@
+#ifndef OAUTH2_H
+#define OAUTH2_H
+
+#include <QString>
+#include <QObject>
+
+class LoginDialog;
+
+class OAuth2 : public QObject
+{
+    Q_OBJECT
+
+public:
+    OAuth2(QWidget* parent = 0);
+    QString accessToken();
+    bool isAuthorized();
+    void startLogin(bool bForce);
+
+    void setScope(const QString& scope);
+    void setClientID(const QString& clientID);
+    void setRedirectURI(const QString& redirectURI);
+    void setCompanyName(const QString& companyName);
+    void setAppName(const QString& appName);
+
+    QString loginUrl();
+
+signals:
+    void loginDone();
+
+private slots:
+    void accessTokenObtained();
+
+private:
+    QString m_strAccessToken;
+
+    QString m_strEndPoint;
+    QString m_strScope;
+    QString m_strClientID;
+    QString m_strRedirectURI;
+    QString m_strResponseType;
+
+    QString m_strCompanyName;
+    QString m_strAppName;
+
+    LoginDialog* m_pLoginDialog;
+};
+
+#endif // OAUTH2_H
